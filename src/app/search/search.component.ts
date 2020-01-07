0b1f92e105c648c316bdf4fd67a5c07149d0b071
BurbaCodes <dilushadasanayaka@gmail.com>
2020-01-07 23:49:59 +0530

    add country list

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -8,6 +8,8 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 })
 export class SearchComponent implements OnInit {
 
+  countryList = [];
+
   constructor(private http: HttpClient) {  
     let params = new HttpParams().set('query',
       `PREFIX geographis: <http://telegraphis.net/ontology/geography/geography#>
@@ -25,7 +27,44 @@ export class SearchComponent implements OnInit {
               })
   }
 
+  
   ngOnInit() {
   }
 
+  getAll() {
+    this.getAllCountries();
+  }
+  getAllCountries() {
+    let query = `
+        PREFIX geographis: <http://telegraphis.net/ontology/geography/geography#>
+        PREFIX gn: <http://www.geonames.org/ontology#>
+        PREFIX measurement: <http://telegraphis.net/ontology/measurement/measurement#>
+        PREFIX metric: <http://www.telegraphis.net/ontology/measurement/metric#> 
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX quantity: <http://www.telegraphis.net/ontology/measurement/quantity#>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX code: <http://www.telegraphis.net/ontology/measurement/code#>
+        
+        select ?name ?alpha ?l ?val
+        where { 
+            ?country geographis:capital ?v .
+            ?country geographis:isoAlpha2 ?alpha .
+            ?country geographis:landArea ?land .
+            ?country gn:name ?name .
+            ?land rdf:value ?l .
+        } limit 100
+    `;
+
+    let params = new HttpParams().set('query',query);
+
+    this.http.get('http://localhost:7200/repositories/countrydb?',{params})
+        .subscribe(data=>{
+          let countries = data['results']['bindings'];
+
+          for( let i = 0; i< countries.length; i++ ){
+            this.countryList.push(countries[i]['name']['value'])
+            console.log('cou',this.countryList);
+          }
+        })
+  }
 }
