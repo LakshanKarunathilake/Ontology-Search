d7fe792d15186597432a6c7d26d4ba9af4ae33e7
BurbaCodes <dilushadasanayaka@gmail.com>
2020-01-07 23:24:21 +0530

    add query

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -9,14 +9,16 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 export class SearchComponent implements OnInit {
 
   constructor(private http: HttpClient) {  
-    let params = new HttpParams().set('query',`PREFIX geographis: <http://telegraphis.net/ontology/geography/geography#>
-    PREFIX gn: <http://www.geonames.org/ontology#>
-    PREFIX country: <http://telegraphis.net/data/countries/AD#>
-    
-    select * 
-    where { 
-        country:AD geographis:currency ?o 
-    } limit 100`)
+    let params = new HttpParams().set('query',
+      `PREFIX geographis: <http://telegraphis.net/ontology/geography/geography#>
+      PREFIX gn: <http://www.geonames.org/ontology#>
+      PREFIX country: <http://telegraphis.net/data/countries/AD#>
+      
+      select * 
+      where { 
+          country:AD geographis:currency ?o 
+      } limit 100`
+    )
     this.http.get('http://localhost:7200/repositories/countrydb?',{params})
               .subscribe(data=> {
                 console.log("d",data);
