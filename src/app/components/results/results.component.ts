b0ace20c5f0ed621acb27f9db811ba66b12ed960
BurbaCodes <dilushadasanayaka@gmail.com>
2020-01-07 22:10:58 +0530

    update

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-results',
@@ -11,6 +11,30 @@ export class ResultsComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  private sparkqlData = null;
+
+  sparkql() {
+    let headers: Headers = new Headers({
+      'Content-type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json'
+    });
+
+    //let params = new URLSearchParams();
+    let query = {'query': 'SELECT * WHERE { <http://dbpedia.org/resource/Awolnation> ?pref ?obj } LIMIT 10'};
+    let format = {'format': 'json'};
+
+    let options = {
+      params: [query,format]
+    };
+
+    this.http.get('http://dbpedia.org/sparql', headers, options) // 1
+      .subscribe(data => {
+          console.log(data);
+          this.sparkqlData = data; // 3
+       });
+     console.log(this.sparkqlData);  // 2
   }
 
 }
